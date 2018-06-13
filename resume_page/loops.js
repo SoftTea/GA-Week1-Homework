53d9fb20e0de249ad06d5f195531f23ef3be07db
Tim Mickle <TimothyRMickle@gmail.com>
2018-06-12 20:41:05 -0500

    Commit 9- Created loops.js file

---
+++ resume_page/loops.js
@@ -0,0 +1,40 @@
+for (let x = 0; x <100; x++) {
+    console.log(x);
+
+}
+
+for (let x = 100; x >= 0; x--) {
+    console.log(x);
+
+}
+
+for (let x = 2; x <= 98; x = x + 2) {
+    console.log(x);
+
+}
+
+for (let x = 98; x >= 2; x = x - 2) {
+    console.log(x);
+
+}
+
+
+for (let x = 98; x >= 2; x = x - 2) {
+    console.log(x);
+
+}
+
+for (let x = 49; x <= 72; x++) {
+    console.log(x);
+
+}
+
+for (let x = 81; x >= 26; x--) {
+    console.log(x);
+
+}
+
+for (let x = 3; x <= 90; x = x + 3) {
+    console.log(x);
+
+}
